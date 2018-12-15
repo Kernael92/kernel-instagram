ce686ecb2bd8dccc83ee473fbfc8186779cae8ea
Kernael92 <kernaelapuko5@gmail.com>
2018-12-15 22:59:01 +0300

    install bootstrap3 and imagekit

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+from django.contrib.auth.models import User,
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    followers = models.ManyToManyField('Profile',related_name="profile_followers",blank=True)
+    following = models.ManyToManyField('Profile',related_name="following_profile", blank=True)
+    profile_pic = ProcessedImageField(upload_to='profile_pics',format="JPEG",options={'quality':100},null=True,blank=True)
+    bio = models.CharField(max_length=200, null=True, blank=True)
+
