f1c9b8b0aad2316bbf20b5360741ee1ae61e3ccd
Kernael92 <kernaelapuko5@gmail.com>
2018-12-15 23:08:11 +0300

    create an Image model class and implement functions to get the number of likes and comments

--- instagram/models.py
+++ instagram/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
 class Profile(models.Model):
@@ -17,4 +18,26 @@ class Profile(models.Model):
             return self.followers.count()
         else:
             return 0
+    def get_number_of_following(self):
+        if self.following.count():
+            return self.following.count()
+        else:
+            return 0 
+    def __str__(self):
+        return self.user.username
 
+class Image(models.Model):
+    user_profile = models.ForeignKey(Profile,null=True, blank=True)
+    image_name = models.CharField(max_length=100)
+    image = ProcessedImageField(upload_to='images/',format='JPEG',options={'quality':100})
+    image_caption = models.CharField(max_length =254)
+    posted_on = models.DateTimeField(default=datetime.now)
+
+    def get_number_of_likes(self):
+        return self.like_set.count()
+    def get_number_of_comments(self):
+        return self.comment_set.count()
+
+    def __str__(self):
+        return self.title
+    
