1b6602b61393d8f68551c84de6d42fe335fef852
Kernael92 <kernaelapuko5@gmail.com>
2018-12-15 23:27:13 +0300

    define an index view function that renders an index template

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Image,Profile,Comment,Profile,Likes
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.contrib.auth.models import User
+from . forms import PostPictureForm, ProfileEditForm, CommentForm
+from django.core.urlresolvers import reverse
+
 
 # Create your views here.
+def index(request):
+    images = image.objects.all()
+    return render(request, 'all-instagram/index.html', {"images":images})
